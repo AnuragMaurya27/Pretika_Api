f93b14d66bd947dcd5f238ca842e6266f8da9e11
Anurag Maurya <anuragkivani@Anurags-MacBook-Air.local>
2026-03-15 20:08:53 +0530

    fix railway port

--- Program.cs
+++ Program.cs
@@ -201,4 +201,5 @@ app.UseAuthorization();
 app.MapControllers();
 app.MapHub<ChatHub>("/hubs/chat");
 
-app.Run();
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Run($"http://0.0.0.0:{port}");
