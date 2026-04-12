6b7c8d3548a5b8cc9bc854237241e52e32bd24df
Anurag27-Blame <kahanikaar563200@gmail.com>
2026-04-12 17:17:58 +0530

    fix: jwt package compatible with net8

--- obj/Debug/net8.0/HauntedVoiceUniverse.AssemblyInfo.cs
+++ obj/Debug/net8.0/HauntedVoiceUniverse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HauntedVoiceUniverse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22f37147ce5c0b310241e4c1981b4e7c17fa2230")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf3c982059e29ddb48ea1c09181c2e0578d04780")]
 [assembly: System.Reflection.AssemblyProductAttribute("HauntedVoiceUniverse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HauntedVoiceUniverse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
